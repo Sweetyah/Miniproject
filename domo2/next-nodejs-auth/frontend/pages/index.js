674939400399185cc2ff2f85b90a18245e4b3e56
Sweetyah.aa@gmail.com <Sweetyah.aa@gmail.com>
2021-04-23 22:54:03 +0700

    third

--- domo2/next-nodejs-auth/frontend/pages/index.js
+++ domo2/next-nodejs-auth/frontend/pages/index.js
@@ -46,16 +46,17 @@ const index = () => {
   return (
     <Layout>
        <Head>
-        <title>Home Page</title>
+        <title>My Portfolio</title>
     </Head>
     <div className={styles.container}><Navbar />
+    
+
       <div className={styles.title}>
-      <marquee bgcolor="#A9E0B8" direction="lefe" scrollamount="5" width="100%"><ins>Welcome to Student Data</ins></marquee></div>
-      <div className={styles.list}>
-        {showStudents()}
+      <marquee bgcolor="#A9E0B8" direction="right" scrollamount="10" width="150%"><ins>My Portfolio</ins></marquee></div>
+     {/*  <div className={styles.list}>
+      {showStudents()}
+      </div> */}
       </div>
-      
-    </div>
     </Layout>
   );
 };
